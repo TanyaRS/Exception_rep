e2ccf6d09684161318441803f6171b5e2738b83b
TanyaRS <Ladiesvest@yandex.ru>
2021-10-12 18:56:03 +0500

    add account,ageexeption,banks

---
+++ src/OutputException/Account.java
@@ -0,0 +1,12 @@
+package OutputException;
+
+public class Account{
+    public String name;
+        public int ages;
+
+        public Account (String name, int ages){
+            this.name=name;
+            this.ages=ages;
+        }
+    }
+
---
+++ src/OutputException/AgeException2.java
@@ -0,0 +1,13 @@
+package OutputException;
+
+ class AgeException2 extends Exception{
+
+
+     private String exc;
+
+     public AgeException2(String exc) {
+
+         this.exc = exc;
+     }
+    }
+
